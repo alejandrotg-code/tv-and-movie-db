cf40dbf00e1c13a5150c6ea31f15a14805581d54
Alejandro Tacoronte González <alejandrotacorontegnlez@gmail.com>
2026-03-03 12:35:32 +0000

    feat: add User model and database configuration

---
+++ src/main/java/com/alejandrotg/tv_and_movie_db/model/User.java
@@ -0,0 +1,29 @@
+package com.alejandrotg.tv_and_movie_db.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.lang.NonNull;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String lastName;
+    private String username;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Rol rol = Rol.USER;
+}
