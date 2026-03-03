6baab7346c8a59cb488d550541ae2a8671a7dfec
Alejandro Tacoronte González <alejandrotacorontegnlez@gmail.com>
2026-03-03 12:01:48 +0000

    feat: initial project setup

---
+++ src/main/java/com/alejandrotg/tv_and_movie_db/TvAndMovieDbApplication.java
@@ -0,0 +1,13 @@
+package com.alejandrotg.tv_and_movie_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvAndMovieDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvAndMovieDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alejandrotg/tv_and_movie_db/TvAndMovieDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandrotg.tv_and_movie_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvAndMovieDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
